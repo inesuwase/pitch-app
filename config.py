d1e33b2159576a062f07d2e60621dfade5f9ce98
ines uwase <uwaseines10@gmail.com>
2019-09-23 10:36:33 +0200

    added a a table

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+class Config:
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:ines@localhost/pitch_app'
+#     UPLOADED_PHOTOS_DEST ='app/static/photos'
+#     #  email configurations
+#     MAIL_SERVER = 'smtp.googlemail.com'
+#     MAIL_PORT = 587
+#     MAIL_USE_TLS = True
+#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+#     @staticmethod
+#     # def init_app(app):
+#     #     pass
+
+
+# class ProdConfig(Config):
+#     pass
+
+# class DevConfig(Config):
+#     DEBUG = True
+
+# config_options = {
+# 'development':DevConfig,
+# 'production':ProdConfig
+# }
