3f0d7f3954dbffb500bf62d71d2a1b446a582519
ines uwase <uwaseines10@gmail.com>
2019-09-23 11:53:24 +0200

    imported the flask form

---
+++ app/main/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField,TextAreaField,RadioField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
+
+
+
+# class PitchForm(FlaskForm):
+# 	title = StringField('Title', validators=[Required()])
+# 	description = TextAreaField("What would you like to pitch ?",validators=[Required()])
+# 	category = RadioField('Label', choices=[ ('promotionpitch','promotionpitch'), ('interviewpitch','interviewpitch'),('pickuplines','pickuplines'),('productpitch','productpitch')],validators=[Required()])
+# 	submit = SubmitField('Submit')
