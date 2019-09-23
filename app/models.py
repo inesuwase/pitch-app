42503cfc73f795ca4d6e8ec58b96db39ad8f0dd9
ines uwase <uwaseines10@gmail.com>
2019-09-23 10:53:59 +0200

    added properties of the user

--- app/models.py
+++ app/models.py
@@ -6,6 +6,9 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    Bio = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
